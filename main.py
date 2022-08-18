a3bea61a8d826aab155afffc6eb2f766f4c5c750
乙烯一个一个一个啊啊啊 <108203157+kevin-wqc@users.noreply.github.com>
2022-08-18 09:19:15 +0800

    are you rickrolled

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-import time
-from dm import dianmao
-from xz import xinzuo
-
-
-
-while True:
-    print('开始检查首页点猫\n')
-    dianmao()
-    
-
-    print('开始检查首页新作\n')
-    xinzuo()
-    
-    print('等待3min')
-
-    time.sleep(180)
-    
+print('''never gonna give you up
+never gonna let you down
+never gonna run around and desert you''')
